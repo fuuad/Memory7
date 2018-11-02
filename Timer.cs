53d91b77de8f4f1d65998bfbcc2e4e20c92e65d7
Speesjaal <43375140+Speesjaal@users.noreply.github.com>
2018-11-02 16:38:01 +0100

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Timer
+{
+    class Timer
+    {
+        /// <summary>
+        /// De variablen worden hier aangemaakt. 
+        /// </summary>
+        private DispatcherTimer dt;
+        private int increment = 0;
+        private string timerLabel;
+
+        /// <summary>
+        /// In deze method word de timer aangemaakt en krijgt hij waardes mee.
+        /// </summary>
+
+        private void StartTimer(object sender, RoutedEventArgs e)
+        {
+            dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += dtTicker;
+            dt.Start();
+        }
+
+        /// <summary>
+        /// Hier telt de timer op en krijgt hij zijn minuten en seconde counter.
+        /// </summary>
+
+        private void dtTicker(object sender, EventArgs e)
+        {
+            increment++;
+            timerLabel = string.Format("{0:d2}:{1:d2}", increment / 60, increment % 60);
+
+        }
+        public void StopTimer()
+        {
+            dt.Stop();
+        }
+
+        /// <summary>
+        /// Hier word de timer gestopt en gereset naar 0. begint dan weer met tellen.
+        /// </summary>
+        private void Resetbutton()
+
+        {
+            dt.Stop();
+            increment = 0;
+            TimerLabel.Content = string.Format("{0:d2}:{1:d2}", increment / 60, increment % 60);
+            dt.Start();
+        }
+
+    }
+}
